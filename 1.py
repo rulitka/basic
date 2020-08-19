424659ceed8290d11a2f3a41010d01cbe28930a7
rulitka <59177208+rulitka@users.noreply.github.com>
2020-08-19 08:26:15 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
-def factorial(n):
+def factorial(N):
     result = 1
-    for i in range(1, n + 1):
+    for i in range(1, N + 1):
         result *= i
         first_number = int(str(abs(result))[0])
     return first_number
