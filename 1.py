25659ff45f810b5993a32b2b54cd28d5b191bff0
rulitka <59177208+rulitka@users.noreply.github.com>
2020-08-19 18:05:34 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
 def squirrel(N):
-    result = 1
-    for i in range(1, N + 1):
-        result *= i
-        first_number = int(str(abs(result))[0])
-    return first_number
+    if N == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, N + 1):
+            result *= i
+            first_number = int(str(abs(result))[0])
+        return first_number
