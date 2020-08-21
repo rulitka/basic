171f08adfdce694d076fcb4140bbefd1657f816b
rulitka <59177208+rulitka@users.noreply.github.com>
2020-08-21 20:23:46 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,16 +1,21 @@
 def odometer(N):
     long = len(N)
     if long >= 2:
+        num = 0
         sum_distance = 0
         distance_all = 0
         for index, number in enumerate(N):
             if index%2 == 0:
                 speed = N[index]
             else:
-                time = N[index]
-                if N[index] == N[-1]:
-                    time = N[-1] - N[-3]
-                distance = speed*time
+                if num == 0:
+                    time = N[index]
+                    num=+1
+                    distance = speed*time  
+                else:
+                    time_new = N[index] - time
+                    time = N[index]
+                    distance = speed*time_new
                 sum_distance += distance
         distance_all += sum_distance
         return distance_all
