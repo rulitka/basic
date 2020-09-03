3c92ff64e7e45452e35c8d957cff27283567cb2c
rulitka <59177208+rulitka@users.noreply.github.com>
2020-09-03 12:47:18 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,10 +1,10 @@
 def SynchronizingTables(N, ids, salary):
-    ids_2 = sorted(ids)
-    salary_2 = sorted(salary)
-    x = list(zip(ids_2,salary_2))
-    lst_new = []
-    for item in range(N):
-        for i in range(N):
-            if ids[item] == x[i][0]:
-                lst_new.append(x[i][1])                           
-        return lst_new
+    ids_2 = sorted(ids)
+    salary_2 = sorted(salary)
+    x = list(zip(ids_2,salary_2))
+    lst_new = []
+    for item in range(N):
+        for i in range(N):
+            if ids[item] == x[i][0]:
+                lst_new.append(x[i][1])
+    return lst_new
