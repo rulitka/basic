d5debf0a2e1af6e363f65b229d612006a1f2467e
rulitka <59177208+rulitka@users.noreply.github.com>
2020-09-14 20:30:41 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -3,10 +3,18 @@ def PatternUnlock(N, hits):
     summ = 0
     sum_all =0
     for i in range(1, len(hits)):
-        if (hits[i] - hits[i-1]) == 1:
+        module = abs(hits[i] - hits[i-1])
+        if module == 1:
             summ =+1
         else:
-            summ =+1.41421356237
+            numbers = [[6, 1, 9], [5, 2, 8], [4, 3, 7]]
+            for y in range(len(numbers)):
+                for j in range(len(numbers)):
+                    if numbers[y][j] == hits[i]:
+                        if numbers[y][j-1] == hits[i-1]:
+                            summ =+1
+                        else:
+                            summ =+1.41421356237
         sum_all +=summ
     string_rounding = float('{:.5f}'.format(sum_all))
     string_convert = str(string_rounding)
