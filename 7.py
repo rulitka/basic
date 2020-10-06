88eab3bdfd57f25d937b2bc55efbb2f0d5523b7e
rulitka <59177208+rulitka@users.noreply.github.com>
2020-10-06 20:50:50 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,48 +1,51 @@
 def WordSearch(width, s, subs):
-    s_new = s.split()  
-    capacity = width # сколько места в текущей строке  
-    cur_s = 0
-    result = []
+    s_new = s.split()
+    new = [""]
+    
     for word in s_new:
-        flag = True if word == subs else False  # искомое слово или нет
-        length = len(word)
-        
-        if capacity == width and capacity >= length: # добавляем в пустую строку и все влезло
-            capacity -= length
-            if flag:
-                result.append(1)
+        if len(word) <= width:
+            if new[-1] == "":
+                new[-1] = word
+            elif len(word) + len(new[-1]) < width:
+                new[-1] += " " + word
             else:
-                result.append(0)
-            continue
-        
-        if capacity != width and capacity > length: # добавляем в непустую строку и все влезло
-            capacity -= length + 1
-            if flag:
-                result[-1] = 1
-            continue 
-        
-        if length <= width: # в текущую строку не влезло и влезло в НОВУЮ пустую
-            cur_s += 1
-            capacity = width - length
-            if flag:
-                result.append(1)
-            else:
-                result.append(0)
-            continue 
-            
-        while length > width:
-            cur_s += 1
-            length -= width
-            if flag:
-                result.append(1)
-            else:
-                result.append(0)   
-                 
-        cur_s += 1
-        capacity -= length 
-        if flag:
-                result.append(1)
+                if len(word) <= width:
+                    new.append(word)
+                else:
+                    new.append(word[:width])
+                    new.append(word[width:])
+       
         else:
-                result.append(0)  
-        
+            if new[-1] == "":
+                new[-1] = word[:width]
+                new.append(word[width:])
+            else:    
+                new.append(word[:width])
+                new.append(word[width:])
+        answer = count(new, subs)
+    return answer
+
+def count(new, subs):
+    k = 0
+    result = []
+    for x in new:
+        if subs in x:
+            for i, t in enumerate(x):
+                if t == subs[k]:
+                    if k == len(subs) - 1:
+                        if t == x[-1]:
+                            result.append(1)
+                            k = 0
+                        elif x[i+1]== ' ':
+                            result.append(1)
+                            k = 0
+                        else:
+                            result.append(0)
+                    else:
+                        k += 1
+                else:
+                    continue    
+        else:
+            k = 0
+            result.append(0)
     return result
