6dce38a7ef00c7f48dd0d9ba129e3a3dd0b9f944
rulitka <59177208+rulitka@users.noreply.github.com>
2020-11-02 22:10:04 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -6,8 +6,8 @@ def TheRabbitsFoot(s, encode):
         square =  round(length_b ** .5)
         sq_2 = square*square
         if sq_2 < length_b:
-            N = square
-            M = square+1    
+            N = square+1
+            M = square    
             sq_new = N*M
             for elem in b:
                 if sq_new > len(b):
@@ -25,17 +25,23 @@ def TheRabbitsFoot(s, encode):
                     b = b + out
                 else:
                     break
-        new_box = [[0]*N for _ in range(M)]
+        new_box = [[0]*M for i in range(N)]
         n = 0
         for i in range(N):
             for j in range(M):
                 new_box[i][j] = b[n]
                 n += 1
         my_string = ''
-        for i in range(N):
-            for j in range(M):
-                my_string += new_box[j][i]
-            my_string += ' '
+        if N == M:
+            for i in range(N):
+                for j in range(M):
+                    my_string += new_box[j][i]
+                my_string += ' '            
+        else:
+            for i in range(N-1):
+                for j in range(M+1):
+                    my_string += new_box[j][i]
+                my_string += ' '
         result = my_string.replace("0", '')
         return result
 
@@ -50,8 +56,10 @@ def TheRabbitsFoot(s, encode):
             if lenght_0 == lenght_i:
                 continue
         my_string = ''
-        for i in range(len(b)):
-            for j in range(len(b[i])):
+        N = lenght_first
+        M = i+1
+        for i in range(N):
+            for j in range(M):
                 my_string += b[j][i]
             my_string += ' '
         my_1 = my_string.replace("0", '')
