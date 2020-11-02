bf4e27b521b3954220c025f52a53223bd270f165
rulitka <59177208+rulitka@users.noreply.github.com>
2020-11-02 21:00:00 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,33 +1,59 @@
-s = 'отдай мою кроличью лапку'
-encode = True
-
 def TheRabbitsFoot(s, encode):
     if encode == True:
         b = s.split()
         b = ''.join(b)
         length_b = len(b)
         square =  round(length_b ** .5)
-        N = square
-        M = square
-        new_box = []
+        sq_2 = square*square
+        if sq_2 < length_b:
+            N = square
+            M = square+1    
+            sq_new = N*M
+            for elem in b:
+                if sq_new > len(b):
+                    out = '0'
+                    b = b + out
+                else:
+                    break
+        if sq_2 >= len(b):
+            N = square
+            M = square
+            sq_new = N*M
+            for elem in b:
+                if sq_new > len(b):
+                    out = '0'
+                    b = b + out
+                else:
+                    break
+        new_box = [[0]*N for _ in range(M)]
+        n = 0
         for i in range(N):
-            new_box.append([])
-            for char in b[i * N: (i + 1) * N]:
-                new_box[-1].append(char)
+            for j in range(M):
+                new_box[i][j] = b[n]
+                n += 1
         my_string = ''
         for i in range(N):
             for j in range(M):
                 my_string += new_box[j][i]
             my_string += ' '
-        return my_string
-TheRabbitsFoot(s, encode)
-# Сначала я превращаю строку в матрицу:
-[['о', 'т', 'д', 'а', 'й'], 
-['м', 'о', 'ю', 'к', 'р'], 
-['о', 'л', 'и', 'ч', 'ь'], 
-['ю', 'л', 'а', 'п', 'к'], 
-['у']]
-# А потом создаю строку my_string в которую хочу добавить все элементы по столбцам.
-У меня успешно создается 'омоюу толл', а потом выходит ошибка:
-IndexError: list index out of range, потому что заканчиваются столбцы,
-и я не знаю, что делать дальше.
+        result = my_string.replace("0", '')
+        return result
+
+    if encode == False:
+        b = s.split()
+        lenght_first = len(b[0])
+        for i in range(len(b)):
+            lenght_0 = len(b[0])
+            lenght_i = len(b[i])
+            if lenght_0 > lenght_i:
+                b[i] = b[i] + '0'
+            if lenght_0 == lenght_i:
+                continue
+        my_string = ''
+        for i in range(len(b)):
+            for j in range(len(b[i])):
+                my_string += b[j][i]
+            my_string += ' '
+        my_1 = my_string.replace("0", '')
+        result = my_1.replace(" ", '')
+        return result
