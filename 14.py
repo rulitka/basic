0e52bc41646923fde4e85c5942d10963731d2022
rulitka <59177208+rulitka@users.noreply.github.com>
2020-11-30 20:38:17 +0300

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,9 +1,12 @@
 def Unmanned(L, N, track):
+    num = 0
     number_of_tr_light = 0
-    real_time = 1
-    for i in range(1, L):
+    real_time = 0
+    for i in range(1, L+1):
         tr_light = track[number_of_tr_light][0]
         if i == tr_light:
+            real_time += 1
+            num += 1
             red = track[number_of_tr_light][1]
             green = track[number_of_tr_light][2]
             color_tl = get_color(real_time,red, green, flag = True)
@@ -11,12 +14,14 @@ def Unmanned(L, N, track):
                 while color_tl =='red':
                     real_time += 1
                     color_tl = get_color(real_time,red, green, flag = True)
+                if color_tl == 'green':
+                    real_time -= 1
             if color_tl == 'green':
-                if number_of_tr_light >= N:
+                if num < N:
                     number_of_tr_light += 1
-                else:
+                    print(number_of_tr_light)
+                if num == N:
                     continue
-                continue
         else:
             real_time += 1
     return real_time
