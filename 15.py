146de05bbcce5db2c12ce3a527296f1e98bbe564
rulitka <59177208+rulitka@users.noreply.github.com>
2020-12-10 20:27:28 +0300

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,6 +1,6 @@
 def TankRush(H1, W1, S1, H2, W2, S2):
     tmp = []
-    new_storke = ''
+    new_stroke = ''
     number = 0
     lumber = 0
     result_S2 = create_matrix(H2, W2, S2)
@@ -8,7 +8,7 @@ def TankRush(H1, W1, S1, H2, W2, S2):
     Flag = False
     for i in range(len(S1)):
         if S1[i] == ' ':
-            new_storke = ''
+            new_stroke = ''
             number = 0
             continue                    
         else:
@@ -17,7 +17,7 @@ def TankRush(H1, W1, S1, H2, W2, S2):
                 simbols = get_simbols(convert_element)
             else:
                 pass
-            new_storke += S1[i]
+            new_stroke += S1[i]
             number += 1
             if number < simbols:
                 Flag = True
@@ -26,18 +26,17 @@ def TankRush(H1, W1, S1, H2, W2, S2):
                 if len(result_S2) == len(tmp):
                     break
                 else:
-                    new_storke_int = int(new_storke)
-                    if  new_storke_int == result_S2[lumber]:
-                        tmp.append(new_storke_int)
-                        new_storke = ''
-                        number -= 1
+                    if  new_stroke == result_S2[lumber]:
+                        tmp.append(new_stroke)
+                        new_stroke = ''
+                        number = 0
                         if lumber < len_result_S2 - 1:
                             lumber += 1
                             Flag = False
                         if lumber == len_result_S2 - 1:
                             continue   
                     else:
-                        new_storke = remove_character(new_storke, 0)
+                        new_stroke = remove_character(new_stroke, 0)
                         number -= 1
                         Flag = True
                         continue
@@ -48,18 +47,18 @@ def TankRush(H1, W1, S1, H2, W2, S2):
        
 def create_matrix(H2, W2, S2):
     result_S2 = []
-    result_S2.append(H2) 
-    result_S2.append(W2)
+    result_S2.append(str(H2))
+    result_S2.append(str(W2))
     S2 = S2.split(" ")
     for i in range(len(S2)):
-        result_S2.append(int(S2[i]))
+        result_S2.append(S2[i])
     return result_S2
 
-def remove_character(new_storke, index):
-    s = list(new_storke)
+def remove_character(new_stroke, index):
+    s = list(new_stroke)
     del s[0]
-    new_storke = "".join(s)
-    return new_storke
+    new_stroke = "".join(s)
+    return new_stroke
 
 def get_simbols(convert_element):
     stroke_element = str(convert_element)
