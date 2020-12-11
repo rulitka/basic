22564fc7ce5f10f85765e47ee384b0612a064223
rulitka <59177208+rulitka@users.noreply.github.com>
2020-12-11 18:07:37 +0300

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,43 +1,45 @@
 def TankRush(H1, W1, S1, H2, W2, S2):
     tmp = []
-    new_stroke = ''
-    number = 0
-    lumber = 0
+    new_storke = ''
+    number_of_elem_new_stroke = 0
+    index_of_elem_result_S2 = 0
+    pre_result_S1 = create_matrix(H1, W1, S1)
+    result_S1 = ' '.join(pre_result_S1)
     result_S2 = create_matrix(H2, W2, S2)
     len_result_S2 = len(result_S2)
     Flag = False
-    for i in range(len(S1)):
-        if S1[i] == ' ':
-            new_stroke = ''
-            number = 0
+    for i in range(len(result_S1)):
+        if result_S1[i] == ' ':
+            new_storke = ''
+            number_of_elem_new_stroke = 0
             continue                    
         else:
             if Flag == False:
-                convert_element = result_S2[lumber]
+                convert_element = result_S2[index_of_elem_result_S2]
                 simbols = get_simbols(convert_element)
             else:
                 pass
-            new_stroke += S1[i]
-            number += 1
-            if number < simbols:
+            new_storke += result_S1[i]
+            number_of_elem_new_stroke += 1
+            if number_of_elem_new_stroke < simbols:
                 Flag = True
                 continue
-            if number == simbols:
+            if number_of_elem_new_stroke == simbols:
                 if len(result_S2) == len(tmp):
                     break
                 else:
-                    if  new_stroke == result_S2[lumber]:
-                        tmp.append(new_stroke)
-                        new_stroke = ''
-                        number = 0
-                        if lumber < len_result_S2 - 1:
-                            lumber += 1
+                    if  new_storke == result_S2[index_of_elem_result_S2]:
+                        tmp.append(new_storke)
+                        new_storke = ''
+                        number_of_elem_new_stroke -= 1
+                        if index_of_elem_result_S2 < len_result_S2 - 1:
+                            index_of_elem_result_S2 += 1
                             Flag = False
-                        if lumber == len_result_S2 - 1:
+                        if index_of_elem_result_S2 == len_result_S2 - 1:
                             continue   
                     else:
-                        new_stroke = remove_character(new_stroke, 0)
-                        number -= 1
+                        new_storke = remove_character(new_storke, 0)
+                        number_of_elem_new_stroke -= 1
                         Flag = True
                         continue
     if result_S2 == tmp:
@@ -45,20 +47,20 @@ def TankRush(H1, W1, S1, H2, W2, S2):
     else:
         return False
        
-def create_matrix(H2, W2, S2):
-    result_S2 = []
-    result_S2.append(str(H2))
-    result_S2.append(str(W2))
-    S2 = S2.split(" ")
-    for i in range(len(S2)):
-        result_S2.append(S2[i])
-    return result_S2
+def create_matrix(H, W, S):
+    result_S = []
+    result_S.append(str(H)) 
+    result_S.append(str(W))
+    S_n = S.split(" ")
+    for i in range(len(S_n)):
+        result_S.append(S_n[i])
+    return result_S
 
-def remove_character(new_stroke, index):
-    s = list(new_stroke)
+def remove_character(new_storke, index):
+    s = list(new_storke)
     del s[0]
-    new_stroke = "".join(s)
-    return new_stroke
+    new_storke = "".join(s)
+    return new_storke 
 
 def get_simbols(convert_element):
     stroke_element = str(convert_element)
