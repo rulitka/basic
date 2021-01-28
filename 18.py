6cbc257d5b2fdfd42db2b9396f6e0cde94cf60a1
rulitka <59177208+rulitka@users.noreply.github.com>
2021-01-28 20:26:45 +0300

    Update 18.py

--- 18.py
+++ 18.py
@@ -9,17 +9,9 @@ def check_list(N, data):
                 continue
             else:
                 for _ in range(0, 2):
-                    if temp_matrix[_] < temp_matrix[_ + 1]:
-                        continue
-                    else:
-                        shift = temp_matrix.pop(_)
-                        temp_matrix.append(shift)
-                        for _ in range(0, 2):
-                            if temp_matrix[_] < temp_matrix[_ + 1]:
-                                continue
-                            else:
-                                shift = temp_matrix.pop(_)
-                                temp_matrix.append(shift)
+                    shift = temp_matrix.pop(0)
+                    temp_matrix.append(shift)
+                    continue
                 data[i] =  temp_matrix[0]
                 data[i + 1] =  temp_matrix[1]
                 data[i + 2] =  temp_matrix[2]
@@ -36,35 +28,23 @@ def check_matrix_one(new_data):
             break
         else:
             Flag = True
-    return Flag
-    
-def check_matrix_two(new_data):
-    Flag = True
-    x = 1 
-    for i in range(1, len(new_data)):
-        if new_data[i] - new_data[i-1] != x:
-            Flag = False
-            break
-        else:
-            Flag = True
-    return Flag       
+    return Flag   
+     
 
 def MisterRobot(N, data):
     Flag = False
-    while Flag == False:    
-        new_data = check_list(N, data)
-        check_matrix_1 = check_matrix_one(new_data)
-        if check_matrix_1 == True:
-            Flag = True
-        else:
+    n = 0
+    while Flag == False:
+        if n <= 200:        
+            new_data = check_list(N, data)
+            n += 1
+            check_matrix_1 = check_matrix_one(new_data)
+            if check_matrix_1 == True:
+                Flag = True
+            else:
+               Flag = False
+               continue
+        if n > 200:
             Flag = False
-            continue
-    if Flag == True:
-        check_matrix_2 = check_matrix_two(new_data)
-        if check_matrix_2 == False:
-            Flag = False
-            exit
-        else:
-            Flag = True
-            exit
+            return Flag
     return Flag
