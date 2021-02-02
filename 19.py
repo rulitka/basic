84df8f1a00213b29fceb37be54a9f3d3b84db062
rulitka <59177208+rulitka@users.noreply.github.com>
2021-02-02 19:40:10 +0300

    Update 19.py

--- 19.py
+++ 19.py
@@ -14,8 +14,9 @@ def sortByName(divide_it):
     newList = sorted(divide_it)
     return newList
 
-def summ_elements(N, inputStrNew):
-    N_new = N
+
+def summ_elements(inputStrNew):
+    N_new = len(inputStrNew)
     z = 0
     for i in range(len(inputStrNew) - 1):
         if z < N_new - 1:
@@ -35,20 +36,28 @@ def sortByLength(new_items):
     inputStrNew = sorted(new_items, key=lambda x: (-x[1], x[0]))
     return inputStrNew
 
+
 def search_dubl(new_items):
     result = False
-    for i in range(len(new_items) - 1):
-        if new_items[i][0] == new_items[i + 1][0]:
-            result = False
-            break
-        else:
-            result = True
+    len_new_items = len(new_items)
+    if len_new_items == 1:
+        result = True
+        exit
+    else:
+        for i in range(len(new_items) - 1):
+            if new_items[i][0] == new_items[i + 1][0]:
+                result = False
+                break
+            else:
+                result = True
     return result
+
 
 def merge_string(inputStrNew):
     for i in range(len(inputStrNew)):
         inputStrNew[i][1] = str(inputStrNew[i][1])
     return inputStrNew 
+
 
 def return_to_start(start_string):
     end_string = []
@@ -62,7 +71,7 @@ def ShopOLAP(N, items):
     inputStrRes = sortByName(divide_it)
     result = False
     while result == False:
-        new_items = summ_elements(N, inputStrRes)
+        new_items = summ_elements(inputStrRes)
         result = search_dubl(new_items)
     inputStrNew = sortByLength(new_items)
     start_string =  merge_string(inputStrNew)
