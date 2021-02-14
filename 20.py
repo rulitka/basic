3ea1937089e6978d8083116cd1f0b8350c926a48
rulitka <59177208+rulitka@users.noreply.github.com>
2021-02-14 12:00:28 +0300

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,19 +1,37 @@
+final_string = ''
+history = []
+position_in_history = -1
+Flag = False
+
+
 def check_string(command):
-    elem =[]
+    elem = []
     len_str = len(command)
+    if len_str < 1:
+        elem = []
     if len_str == 1:
         for i in range(len(command)):
-            elem.append(int(command[i]))
+            if command[i].isdigit() == True:
+                elem.append(int(command[i]))
+            else:
+                elem.append((command[i]))
     if len_str > 1:
         elem_temp = command.split(" ", 1)
         for i in range(len(elem_temp)):
             if i == 0:
-                elem.append(int(elem_temp[i]))
-                if elem[0] == 1:
-                    elem.append(elem_temp[1])
+                check_elem = elem_temp[i]
+                if check_elem.isdigit() == True:
+                        elem.append(int(elem_temp[i]))
+                        if elem[0] == 1:
+                            elem.append(elem_temp[1])
+                        if 1 < elem[0] < 6:
+                            elem.append(int(elem_temp[1]))
+                        if elem[0] >= 6:
+                            elem.append((elem_temp[1]))
                 else:
-                    elem.append(int(elem_temp[1]))
+                    elem = elem_temp
     return elem
+
 
 def reset_history(history, position_in_history):
     history_new = []
@@ -25,63 +43,100 @@ def reset_history(history, position_in_history):
     history = history_new
     return history
 
+
+def add_string(check_str, Flag):
+    global final_string
+    global history
+    global position_in_history
+    if Flag == False:
+        final_string += check_str[1]
+        history.append(final_string)
+        position_in_history += 1
+    if Flag == True:
+        final_string += check_str[1]
+        history = reset_history(history, position_in_history)
+        history.append(final_string)
+        position_in_history -= 1
+    return final_string
+
+
+def del_elements(check_str, Flag):
+    global final_string
+    global history
+    global position_in_history
+    if Flag == False:
+        n = check_str[1]
+        len_str = len(final_string)
+        if n > len_str:
+            final_string = ' '
+            exit
+        else:
+            while n:
+                final_string = final_string[:-1]
+                n -= 1
+            history.append(final_string)
+            position_in_history += 1       
+    if Flag == True:
+        n = check_str[1]
+        while n:
+            final_string = final_string[:-1]
+            n -= 1
+        history = reset_history(history, position_in_history)
+        history.append(final_string)
+        position_in_history += 1
+    return final_string
+
+
+def get_index_element(check_str):
+    global final_string
+    index_el = check_str[1]
+    len_str = len(final_string)
+    if index_el > len_str:
+        final_string = ' '
+        exit
+    else:
+        final_string = str(final_string[index_el])
+    return final_string
+
+
+def del_position():
+    global final_string
+    global history
+    global position_in_history
+    global Flag
+    if position_in_history == 0:
+        position_in_history = 0
+        final_string = history[position_in_history]
+    if position_in_history != 0:
+        position_in_history -= 1
+        final_string = history[position_in_history]
+    Flag = True
+    return final_string
+
+
+def return_position(check_str):
+    global final_string
+    global history
+    global position_in_history
+    if history[position_in_history] == history[-1]:
+        final_string = history[position_in_history]
+    else:
+        position_in_history += 1
+        final_string = history[position_in_history]
+    return final_string
+
+
 def BastShoe(command):
-    final_string = ''
-    history = []
-    position_in_history = -1
-    Flag = False
-    for i in range(len(command)):
-        check_str = check_string(command[i])
-        if check_str[0] == 1:
-            if Flag == False:
-                final_string += check_str[1]
-                history.append(final_string)
-                position_in_history += 1
-            if Flag == True:
-                final_string += check_str[1]
-                history = reset_history(history, position_in_history)
-                history.append(final_string)	
-        if check_str[0] == 2:
-            if Flag == False:
-                n = check_str[1]
-                len_str = len(final_string)
-                if n > len_str:
-                    final_string = ' '
-                    exit
-                else:
-                    while n:
-                        final_string = final_string[:-1]
-                        n -= 1
-                        history.append(final_string)
-                    position_in_history += 1        
-            if Flag == True:
-                n = check_str[1]
-                while n:
-                    final_string = final_string[:-1]
-                    n -= 1
-                history = reset_history(history, position_in_history)
-                history.append(final_string)
-                position_in_history += 1
-        if check_str[0] == 3:
-            index_el = check_str[1]
-            len_str = len(final_string)
-            if index_el > len_str:
-                final_string = ' '
-                exit
-            else:
-                final_string = str(final_string[index_el])
-        if check_str[0] == 4:
-            if position_in_history == 0:
-                position_in_history = 0
-                final_string = history[position_in_history]            	
-            if position_in_history != 0:	
-                position_in_history -= 1
-                final_string = history[position_in_history]
-            Flag = True
-        if check_str[0] == 5:
-            if history[position_in_history] == history[-1]:
-                final_string = history[position_in_history]
-            else:
-                position_in_history += 1
-                final_string = history[position_in_history]
+    check_str = check_string(command)
+    global final_string
+    if check_str[0] == 1:
+        final_string = add_string(check_str, Flag)
+    if check_str[0] == 2:
+        final_string = del_elements(check_str, Flag)
+    if check_str[0] == 3:
+        final_string = get_index_element(check_str)
+    if check_str[0] == 4:
+        final_string = del_position()
+    if check_str[0] == 5:
+        final_string = return_position(check_str)
     return final_string
