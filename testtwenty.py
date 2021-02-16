740354e175f437fc28de9e2f0043499afc2cfc98
rulitka <59177208+rulitka@users.noreply.github.com>
2021-02-16 13:57:57 +0300

    Update testtwenty.py

--- testtwenty.py
+++ testtwenty.py
@@ -4,14 +4,14 @@ import twenty
 class MyTests(unittest.TestCase):
     
     def test_add(self):
-        self.assertEqual(twenty.BastShoe('1 Привет'), 'Привет')
-        self.assertEqual(twenty.BastShoe('1 , Мир!'), 'Привет, Мир!')
-        self.assertEqual(twenty.BastShoe('1 ++'), 'Привет, Мир!++')
-        self.assertEqual(twenty.BastShoe('3 0'), 'П')
-        self.assertEqual(twenty.BastShoe('3 1'), 'р')
-        self.assertEqual(twenty.BastShoe('3 2'), 'и')
-        self.assertEqual(twenty.BastShoe('3 3'), 'в')
-        self.assertEqual(twenty.BastShoe('3 100'), '')
+        self.assertEqual(twenty.BastShoe('1 a'), 'a')
+        self.assertEqual(twenty.BastShoe('1 b'), 'ab')
+        self.assertEqual(twenty.BastShoe('1 c'), 'abc')
+        self.assertEqual(twenty.BastShoe('1 d'), 'abcd')
+        self.assertEqual(twenty.BastShoe('1 e'), 'abcde')
+        self.assertEqual(twenty.BastShoe('3 5'), '')
+        #self.assertEqual(twenty.BastShoe('4'), 'abcd')
+        #self.assertEqual(twenty.BastShoe('5'), 'abcde')
 
 if __name__ == '__main__':
     unittest.main()
