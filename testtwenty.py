6852c8519d95f4b3560e0d8e5adca9358a5a2b23
rulitka <59177208+rulitka@users.noreply.github.com>
2021-02-16 20:17:05 +0300

    Update testtwenty.py

--- testtwenty.py
+++ testtwenty.py
@@ -3,15 +3,47 @@ import twenty
 
 class MyTests(unittest.TestCase):
     
-    def test_add(self):
+    def test_one(self):
+        self.assertEqual(twenty.BastShoe('1 Привет'), 'Привет')
+        self.assertEqual(twenty.BastShoe('1 , Мир!'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('1 ++'), 'Привет, Мир!++')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет')
+        self.assertEqual(twenty.BastShoe('5'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет')
+        self.assertEqual(twenty.BastShoe('5'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('5'), 'Привет, Мир!++')
+        self.assertEqual(twenty.BastShoe('5'), 'Привет, Мир!++')
+        self.assertEqual(twenty.BastShoe('5'), 'Привет, Мир!++')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет')
+        self.assertEqual(twenty.BastShoe('2 2'), 'Прив')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет')
+        self.assertEqual(twenty.BastShoe('5'), 'Прив')
+        self.assertEqual(twenty.BastShoe('5'), 'Прив')
+        self.assertEqual(twenty.BastShoe('5'), 'Прив')
+
+    def test_two(self):
+        self.assertEqual(twenty.BastShoe('1 Привет'), 'Привет')
+        self.assertEqual(twenty.BastShoe('1 , Мир!'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('1 ++'), 'Привет, Мир!++')
+        self.assertEqual(twenty.BastShoe('2 2'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет, Мир!++')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('1 *'), 'Привет, Мир!*')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('4'), 'Привет, Мир!')
+        self.assertEqual(twenty.BastShoe('3 6'), ',')
+        self.assertEqual(twenty.BastShoe('2 100'), '')
+       
+    def test_three(self):
         self.assertEqual(twenty.BastShoe('1 a'), 'a')
         self.assertEqual(twenty.BastShoe('1 b'), 'ab')
         self.assertEqual(twenty.BastShoe('1 c'), 'abc')
         self.assertEqual(twenty.BastShoe('1 d'), 'abcd')
         self.assertEqual(twenty.BastShoe('1 e'), 'abcde')
         self.assertEqual(twenty.BastShoe('3 5'), '')
-        #self.assertEqual(twenty.BastShoe('4'), 'abcd')
-        #self.assertEqual(twenty.BastShoe('5'), 'abcde')
 
 if __name__ == '__main__':
     unittest.main()
